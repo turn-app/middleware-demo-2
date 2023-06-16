6966bbb173b8c8e69ebd73a7b3bf78056887bf35
Haochen Wei <haochen@turn.app>
2023-06-16 13:39:52 -0700

    Update middleware setup

---
+++ instrumentation.ts
@@ -0,0 +1,13 @@
+// @ts-ignore 
+import tracker from '@middleware.io/agent-apm-nextjs';
+
+export function register() {
+    tracker.track({
+        projectName: "turn-demo-dev-2",
+        serviceName: "turn-demo-dev-2",
+        accountKey: "tsxvgwovbtqqojghcmkvfwgscjzwdreopzed",
+        target: "https://qsvae.middleware.io:443",
+    });
+
+    tracker.info('ONE TIME INSTRUMENTATION DONE dev 2');
+}
